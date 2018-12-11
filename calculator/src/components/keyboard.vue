bc44ecc48a1231774d7779cb14f3bee2f3854391
SupBuddy <troyyoung@live.cn>
2018-12-11 15:54:11 +0800

    init

---
+++ calculator/src/components/keyboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="page">
+        <div :value='value'></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'keyboard',
+    props: ['value'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
